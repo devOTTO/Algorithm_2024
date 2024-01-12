130ff913dd2c85d1d99cfd1074402236147bcf40
Yoonmee Hwang <yoongotto@gmail.com>
2024-01-12 23:38:56 +0900

    Create 674.cpp

---
+++ leetcode/Top_Interview_Questions_Easy_Array/674.cpp
@@ -0,0 +1,36 @@
+//
+// Created by devOTTO on 2024-01-12.
+//
+//Leetcode - Top Intervie Questions - Easy - Array - Intersection of Two Arrays II
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+        int i = 0;
+        int j = 0;
+        
+        vector<int> result;
+        while(i < size1 && j < size2)
+        {
+            
+           if(nums1[i] == nums2[j]){
+                result.push_back(nums1[i]);
+                i++;
+				j++;
+            }
+            else if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+        }
+        return result;
+    }
+};
