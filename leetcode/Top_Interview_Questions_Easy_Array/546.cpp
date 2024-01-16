7f7431648846707371a40df0d2bacfac8b878267
Yoonmee Hwang <yoongotto@gmail.com>
2024-01-16 16:06:16 +0900

    Update 546.cpp

--- leetcode/Top_Interview_Questions_Easy_Array/546.cpp
+++ leetcode/Top_Interview_Questions_Easy_Array/546.cpp
@@ -2,4 +2,21 @@
 // Created by devOTTO on 2024-01-15.
 //
 //Leetcode - Top Intervie Questions - Easy - Array - Two Sums
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int size = nums.size();
+
+        for (int i = 0; i < size; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; 
+    }
+};
 
